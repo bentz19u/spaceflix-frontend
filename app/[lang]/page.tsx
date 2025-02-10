1a0b3aa664d432f34fa620935e14b37952ed9988
Daniel Bentz <db.daniel.bentz@gmail.com>
2025-02-10 15:45:40 +0900

    added form validation on the login form

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from '@/app/ui/login-form';
 export default async function Home() {
   return (
     <div className='flex justify-center pt-2'>
-      <div className='h-[500px] min-w-[400px] rounded-lg bg-neutral-900/90 pr-10 pl-10'>
+      <div className='h-[600px] min-w-[400px] rounded-lg bg-neutral-900/90 pr-10 pl-10'>
         <LoginForm />
       </div>
     </div>
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -2,9 +2,14 @@
 
 import { FormEvent, useState } from 'react';
 import { clientAuthorizedFetcher } from '@/app/lib/client-authorized-fetch-lib';
+import { z } from 'zod';
+
+const emailSchema = z.string().email({ message: 'Please enter a valid email.' });
+const passwordSchema = z.string().min(8, 'Password must be at least 8 characters.');
 
 export default function LoginForm() {
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [formErrors, setFormErrors] = useState<{ [key: string]: string }>({});
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
@@ -36,6 +41,16 @@ export default function LoginForm() {
     }
   }
 
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    const schema = name === 'email' ? emailSchema : passwordSchema;
+    const result = schema.safeParse(value);
+    setFormErrors((prev) => ({
+      ...prev,
+      [name]: result.success ? '' : result.error.issues[0].message,
+    }));
+  };
+
   async function handleRouteHandlerClick() {
     const data = await Promise.all([
       clientAuthorizedFetcher('/api/auth/test-access-token', 'GET'),
@@ -45,28 +60,21 @@ export default function LoginForm() {
   }
 
   return (
-    <form
-      id='login-form'
-      onSubmit={handleSubmit}
-      className='flex h-full flex-col justify-between py-10'
-    >
+    <form id='login-form' onSubmit={handleSubmit} className='flex h-full flex-auto flex-col py-10'>
       <header className='mb-5 min-h-10'>Sign In</header>
 
-      {errorMessage && (
-        <div className='rounded-lg bg-yellow-500 p-2.5 text-black'>
-          {errorMessage}
-        </div>
-      )}
+      {errorMessage && <div className='mb-5 rounded-lg bg-yellow-500 p-2.5 text-black'>{errorMessage}</div>}
 
-      <div className='flex flex-grow-[0.5] flex-col justify-between'>
-        <div className='relative'>
+      <div className='flex flex-col'>
+        <div className='relative mb-5'>
           <input
             type='text'
             id='email'
             name='email'
-            className='block w-full appearance-none rounded-lg border-0 border-white bg-blue-950 px-2.5 pt-5 pb-2.5 text-sm text-white focus:ring-2 focus:ring-white'
+            className={`peer block w-full appearance-none rounded-lg border-0 border-white bg-blue-950 px-2.5 pt-5 pb-2.5 text-sm text-white focus:ring-2 focus:ring-white ${formErrors.email ? 'ring-2 ring-red-500' : ''}`}
             defaultValue='daniel.bentz@test.com'
-            placeholder=' '
+            placeholder=''
+            onBlur={handleBlur}
           />
           <label
             htmlFor='email'
@@ -74,16 +82,40 @@ export default function LoginForm() {
           >
             Email
           </label>
+          {formErrors.email && (
+            <div className='flex flex-row items-center text-red-500'>
+              <div className='mr-1'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  role='img'
+                  viewBox='0 0 16 16'
+                  width='16'
+                  height='16'
+                  data-icon='CircleXSmall'
+                >
+                  <path
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    d='M14.5 8C14.5 11.5899 11.5899 14.5 8 14.5C4.41015 14.5 1.5 11.5899 1.5 8C1.5 4.41015 4.41015 1.5 8 1.5C11.5899 1.5 14.5 4.41015 14.5 8ZM16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM4.46967 5.53033L6.93934 8L4.46967 10.4697L5.53033 11.5303L8 9.06066L10.4697 11.5303L11.5303 10.4697L9.06066 8L11.5303 5.53033L10.4697 4.46967L8 6.93934L5.53033 4.46967L4.46967 5.53033Z'
+                    fill='currentColor'
+                  ></path>
+                </svg>
+              </div>
+              {formErrors.email}
+            </div>
+          )}
         </div>
 
-        <div className='relative'>
+        <div className='relative mb-5'>
           <input
             type='password'
             id='password'
             name='password'
-            className='block w-full appearance-none rounded-lg border-0 border-white bg-blue-950 px-2.5 pt-5 pb-2.5 text-sm text-white focus:ring-2 focus:ring-white'
+            className={`peer block w-full appearance-none rounded-lg border-0 border-white bg-blue-950 px-2.5 pt-5 pb-2.5 text-sm text-white focus:ring-2 focus:ring-white ${formErrors.password ? 'ring-2 ring-red-500' : ''}`}
             defaultValue='password'
-            placeholder=' '
+            placeholder=''
+            onBlur={handleBlur}
           />
           <label
             htmlFor='password'
@@ -91,19 +123,33 @@ export default function LoginForm() {
           >
             Password
           </label>
+          {formErrors.password && (
+            <div className='mt-1 flex flex-row items-center text-red-500'>
+              <div className='mr-1'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  role='img'
+                  viewBox='0 0 16 16'
+                  width='16'
+                  height='16'
+                  data-icon='CircleXSmall'
+                >
+                  <path
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    d='M14.5 8C14.5 11.5899 11.5899 14.5 8 14.5C4.41015 14.5 1.5 11.5899 1.5 8C1.5 4.41015 4.41015 1.5 8 1.5C11.5899 1.5 14.5 4.41015 14.5 8ZM16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM4.46967 5.53033L6.93934 8L4.46967 10.4697L5.53033 11.5303L8 9.06066L10.4697 11.5303L11.5303 10.4697L9.06066 8L11.5303 5.53033L10.4697 4.46967L8 6.93934L5.53033 4.46967L4.46967 5.53033Z'
+                    fill='currentColor'
+                  ></path>
+                </svg>
+              </div>
+              {formErrors.password}
+            </div>
+          )}
         </div>
 
-        <label
-          htmlFor='rememberMe'
-          className='flex cursor-pointer items-center gap-2'
-        >
-          <input
-            type='checkbox'
-            id='rememberMe'
-            name='rememberMe'
-            defaultChecked={true}
-            className='peer hidden'
-          />
+        <label htmlFor='rememberMe' className='mb-5 flex cursor-pointer items-center gap-2'>
+          <input type='checkbox' id='rememberMe' name='rememberMe' defaultChecked={true} className='peer hidden' />
           <div
             className='flex h-4 w-4 items-center justify-center rounded-sm border transition-colors duration-200 peer-checked:bg-white'
             aria-hidden='true'
@@ -142,11 +188,7 @@ export default function LoginForm() {
 
       <footer className='mt-5 min-h-10'>
         <p>New to Spaceflix? Sign up now.</p>
-        <button
-          type='button'
-          onClick={handleRouteHandlerClick}
-          className='mt-4 cursor-pointer'
-        >
+        <button type='button' onClick={handleRouteHandlerClick} className='mt-4 cursor-pointer'>
           Test Access token (By route handler)
         </button>
       </footer>
