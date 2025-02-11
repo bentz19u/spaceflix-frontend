8bb02ccd9bbf47a87bd515c1cd5a627e95a6c28e
Daniel Bentz <db.daniel.bentz@gmail.com>
2025-02-11 15:00:09 +0900

    extracted the input error into another conponent

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -3,6 +3,7 @@
 import { FormEvent, useState } from 'react';
 import { clientAuthorizedFetcher } from '@/app/lib/client-authorized-fetch-lib';
 import { z } from 'zod';
+import FormInputError from '@/app/ui/form-input-error';
 
 const emailSchema = z.string().email({ message: 'Please enter a valid email.' });
 const passwordSchema = z.string().min(8, 'Password must be at least 8 characters.');
@@ -51,6 +52,7 @@ export default function LoginForm() {
     }));
   };
 
+  // only to test the race conditions while refreshing tokens
   async function handleRouteHandlerClick() {
     const data = await Promise.all([
       clientAuthorizedFetcher('/api/auth/test-access-token', 'GET'),
@@ -82,29 +84,7 @@ export default function LoginForm() {
           >
             Email
           </label>
-          {formErrors.email && (
-            <div className='flex flex-row items-center text-red-500'>
-              <div className='mr-1'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  role='img'
-                  viewBox='0 0 16 16'
-                  width='16'
-                  height='16'
-                  data-icon='CircleXSmall'
-                >
-                  <path
-                    fillRule='evenodd'
-                    clipRule='evenodd'
-                    d='M14.5 8C14.5 11.5899 11.5899 14.5 8 14.5C4.41015 14.5 1.5 11.5899 1.5 8C1.5 4.41015 4.41015 1.5 8 1.5C11.5899 1.5 14.5 4.41015 14.5 8ZM16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM4.46967 5.53033L6.93934 8L4.46967 10.4697L5.53033 11.5303L8 9.06066L10.4697 11.5303L11.5303 10.4697L9.06066 8L11.5303 5.53033L10.4697 4.46967L8 6.93934L5.53033 4.46967L4.46967 5.53033Z'
-                    fill='currentColor'
-                  ></path>
-                </svg>
-              </div>
-              {formErrors.email}
-            </div>
-          )}
+          {formErrors.email && <FormInputError errorMessage={formErrors.email} />}
         </div>
 
         <div className='relative mb-5'>
@@ -123,29 +103,7 @@ export default function LoginForm() {
           >
             Password
           </label>
-          {formErrors.password && (
-            <div className='mt-1 flex flex-row items-center text-red-500'>
-              <div className='mr-1'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  role='img'
-                  viewBox='0 0 16 16'
-                  width='16'
-                  height='16'
-                  data-icon='CircleXSmall'
-                >
-                  <path
-                    fillRule='evenodd'
-                    clipRule='evenodd'
-                    d='M14.5 8C14.5 11.5899 11.5899 14.5 8 14.5C4.41015 14.5 1.5 11.5899 1.5 8C1.5 4.41015 4.41015 1.5 8 1.5C11.5899 1.5 14.5 4.41015 14.5 8ZM16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM4.46967 5.53033L6.93934 8L4.46967 10.4697L5.53033 11.5303L8 9.06066L10.4697 11.5303L11.5303 10.4697L9.06066 8L11.5303 5.53033L10.4697 4.46967L8 6.93934L5.53033 4.46967L4.46967 5.53033Z'
-                    fill='currentColor'
-                  ></path>
-                </svg>
-              </div>
-              {formErrors.password}
-            </div>
-          )}
+          {formErrors.password && <FormInputError errorMessage={formErrors.password} />}
         </div>
 
         <label htmlFor='rememberMe' className='mb-5 flex cursor-pointer items-center gap-2'>
@@ -174,7 +132,7 @@ export default function LoginForm() {
         </label>
         <button
           type='submit'
-          className='min-h-10 cursor-pointer rounded-lg bg-red-600 disabled:bg-red-800'
+          className='min-h-10 cursor-pointer rounded-lg bg-red-600 transition-colors duration-200 disabled:bg-red-800'
           disabled={isLoading}
           aria-busy={isLoading}
         >
