32ceabe379a05f2eec65871bd682f273c89c9371
Daniel Bentz <db.daniel.bentz@gmail.com>
2025-02-20 14:31:57 +0900

    improved CSS of main page registration form

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,6 +1,7 @@
 import FormInputError from '@/app/ui/form-input-error';
 import React from 'react';
 import Link from 'next/link';
+import cn from '@/app/lib/cn';
 
 export default async function Home() {
   return (
@@ -14,26 +15,33 @@ export default async function Home() {
         <div className='mb-4'>
           Ready to watch? Enter your email to create or restart your membership.
         </div>
-        <div className='flex flex-row'>
-          <div className='relative mr-2'>
+        <div className={cn('flex flex-col', 'sm:flex-row')}>
+          <div className='relative mr-2 w-full'>
             <input
               type='email'
               id='email'
               name='email'
-              className={`peer block w-full appearance-none rounded-lg border-0 border-white bg-blue-950 px-2.5 pt-5 pb-2.5 text-sm text-white focus:ring-2 focus:ring-white`}
+              className={cn(
+                'peer block w-full rounded-lg border-1 border-gray-400 bg-neutral-900/90 px-2.5 pt-5 pb-2.5 text-sm text-white',
+                'focus:ring-2 focus:ring-white focus:outline-none'
+              )}
               defaultValue='db.daniel.bentz@gmail.com'
               placeholder=''
             />
             <label
               htmlFor='email'
-              className='absolute start-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:text-blue-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-blue-500'
+              className={cn(
+                'absolute start-2.5 top-4 z-10 mt-0 origin-[0] -translate-y-4 scale-75 transform text-sm text-gray-500 duration-300',
+                'peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100',
+                'peer-focus:-translate-y-4 peer-focus:scale-75'
+              )}
             >
               Email address
             </label>
           </div>
           <Link
             href='/register'
-            className='flex h-full min-w-15 items-center justify-center rounded-lg bg-red-600 px-3'
+            className='flex h-full min-w-15 items-center justify-center rounded-lg bg-red-600 px-3 whitespace-nowrap'
           >
             Get started
           </Link>
