a1863e241bc8d3a8aa02c9243e81a3a84d908adf
Daniel Bentz <db.daniel.bentz@gmail.com>
2025-03-10 14:27:49 +0900

    moved home register input to a client component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import Link from 'next/link';
 import cn from '@/app/lib/cn';
 
-import ArrowRight from '@/app/assets/icons/arrow-right.svg';
+import HomeRegister from '@/app/ui/home-register';
 
 export default async function Home() {
   return (
@@ -18,42 +17,7 @@ export default async function Home() {
         <div className='mb-4'>
           Ready to watch? Enter your email to create or restart your membership.
         </div>
-        <div className={cn('flex flex-col gap-5', 'sm:flex-row sm:gap-0')}>
-          <div className='relative mr-2 h-14 w-full'>
-            <input
-              type='email'
-              id='email'
-              name='email'
-              className={cn(
-                'peer block h-full w-full rounded-lg border-1 border-gray-400 bg-neutral-900/90 px-2.5 pt-5 pb-2.5 text-sm text-white',
-                'focus:ring-2 focus:ring-white focus:outline-none'
-              )}
-              defaultValue='db.daniel.bentz@gmail.com'
-              placeholder=''
-            />
-            <label
-              htmlFor='email'
-              className={cn(
-                'absolute start-2.5 top-5 z-10 mt-0 origin-[0] -translate-y-4 scale-75 transform text-sm text-gray-500 duration-300',
-                'peer-placeholder-shown:top-4 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100',
-                'peer-focus:top-5 peer-focus:-translate-y-4 peer-focus:scale-75'
-              )}
-            >
-              Email address
-            </label>
-          </div>
-          <Link
-            href='/register'
-            className={cn(
-              'mx-auto flex h-14 min-w-20 items-center justify-center rounded-lg bg-red-600 px-3 text-lg font-bold whitespace-nowrap',
-              'sm:mx-0 sm:min-w-36',
-              'lg:min-w-56 lg:text-2xl'
-            )}
-          >
-            <span className={cn('mr-2', 'lg:mr-4')}>Get started</span>
-            <ArrowRight />
-          </Link>
-        </div>
+        <HomeRegister />
       </div>
     </div>
   );
